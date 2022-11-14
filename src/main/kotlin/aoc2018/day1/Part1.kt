e3d028f6b0677786ec10916487675a24eed678ca
John Vinh <john.michael.vinh2@gmail.com>
2022-11-14 17:22:24 -0500

    Solve 2018 day 1 part 1

--- src/main/kotlin/aoc2018/day1/Part1.kt
+++ src/main/kotlin/aoc2018/day1/Part1.kt
@@ -1,2 +1,32 @@
 package aoc2018.day1
 
+import dev.johnvinh.getInput
+
+fun main() {
+    val regex = Regex("^([+-])([0-9]+)$")
+    val input = getInput()
+    var frequency = 0
+    for (line in input) {
+        val result = regex.matchEntire(line)
+        if (result != null) {
+            val operator = result.groups[1]?.value
+            val value = result.groups[2]?.value
+            var number = 0
+            try {
+                if (value != null) {
+                    number = value.toInt()
+                }
+            } catch (e: NumberFormatException) {
+                e.printStackTrace()
+            }
+            if (operator != null) {
+                if (operator == "+") {
+                    frequency += number
+                } else {
+                    frequency -= number
+                }
+            }
+        }
+    }
+    println(frequency)
+}
