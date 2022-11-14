1d987e84c75ea2d37f40a055d0d947ea0cc47157
John Vinh <john.michael.vinh2@gmail.com>
2022-11-14 16:47:49 -0500

    Finish HandleFileInput

--- src/main/kotlin/dev/johnvinh/HandleFileInput.kt
+++ src/main/kotlin/dev/johnvinh/HandleFileInput.kt
@@ -1,2 +1,12 @@
 package dev.johnvinh
 
+import java.io.BufferedReader
+import java.io.FileReader
+
+fun getInput(): List<String> {
+    FileReader("input.txt").use { itFr ->
+        BufferedReader(itFr).use {
+            return it.readLines()
+        }
+    }
+}
