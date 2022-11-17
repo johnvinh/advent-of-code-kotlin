027c11df42cccf6f21b9157bd07d29010b3bc1db
John Vinh <john.michael.vinh2@gmail.com>
2022-11-17 11:40:01 -0500

    Solve 2018 day 1 part 2

--- src/main/kotlin/aoc2018/day1/Part2.kt
+++ src/main/kotlin/aoc2018/day1/Part2.kt
@@ -1,2 +1,42 @@
 package aoc2018.day1
 
+import dev.johnvinh.getInput
+
+fun main() {
+    val valuesSeen = HashSet<Int>()
+    val regex = Regex("^([+-])([0-9]+)$")
+    val input = getInput()
+    var frequency = 0
+    var duplicate = false
+    while (!duplicate) {
+        for (line in input) {
+            val result = regex.matchEntire(line)
+            if (result != null) {
+                val operator = result.groups[1]?.value
+                val value = result.groups[2]?.value
+                var number = 0
+                try {
+                    if (value != null) {
+                        number = value.toInt()
+                    }
+                } catch (e: NumberFormatException) {
+                    e.printStackTrace()
+                }
+                if (operator != null) {
+                    if (operator == "+") {
+                        frequency += number
+                    } else {
+                        frequency -= number
+                    }
+                }
+            }
+            if (valuesSeen.contains(frequency)) {
+                println(frequency)
+                duplicate = true
+                return
+            } else {
+                valuesSeen.add(frequency)
+            }
+        }
+    }
+}
