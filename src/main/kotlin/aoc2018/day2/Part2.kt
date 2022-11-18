39535f096b33d7bf31a092285505c50bf7625af7
John Vinh <john.michael.vinh2@gmail.com>
2022-11-18 13:38:53 -0500

    Solve 2018 day 2 part 2

--- src/main/kotlin/aoc2018/day2/Part2.kt
+++ src/main/kotlin/aoc2018/day2/Part2.kt
@@ -1,2 +1,29 @@
 package aoc2018.day2
 
+import dev.johnvinh.getInput
+
+fun main() {
+    val lines = getInput()
+    for ((i, line) in lines.withIndex()) {
+
+        for ((j, nextLine) in lines.subList(i+1, lines.size).withIndex()) {
+            var numSame = 0
+            // Each char in line
+            for (k in line.indices) {
+                if (line[k] == nextLine[k]) {
+                    numSame++
+                }
+            }
+            // Found the 2 lines
+            if (numSame == line.length-1) {
+                // Find their difference
+                for (k in line.indices) {
+                    if (line[k] == nextLine[k]) {
+                        print(line[k])
+                    }
+                }
+                return
+            }
+        }
+    }
+}
