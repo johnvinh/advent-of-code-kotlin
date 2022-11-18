73748a6bc9ba1f1bd11b86abcc46a27263067f32
John Vinh <john.michael.vinh2@gmail.com>
2022-11-18 10:21:10 -0500

    Solve 2018 day 2 part 1

--- src/main/kotlin/aoc2018/day2/Part1.kt
+++ src/main/kotlin/aoc2018/day2/Part1.kt
@@ -1,2 +1,32 @@
 package aoc2018.day2
 
+import dev.johnvinh.getInput
+
+fun main() {
+    var num3 = 0
+    var num2 = 0
+    val lines = getInput()
+    for (line in lines) {
+        val occurrences = HashMap<Char, Int>()
+        val charsAdded = HashSet<Char>()
+        var countedForTwo = false
+        var countedForThree = false
+
+        for (char in line) {
+            occurrences[char] = occurrences.getOrDefault(char, 0) + 1
+        }
+        for (char in line) {
+            if (!countedForTwo && occurrences.getOrDefault(char, 0) == 2 && !charsAdded.contains(char)) {
+                num2++
+                charsAdded.add(char)
+                countedForTwo = true
+            }
+            if (!countedForThree && occurrences.getOrDefault(char, 0) == 3 && !charsAdded.contains(char)) {
+                num3++
+                charsAdded.add(char)
+                countedForThree = true
+            }
+        }
+    }
+    println(num2 * num3)
+}
