d2dc2b21b1b37d10f6d6076ec5f58f9dd59dfb22
John Vinh <john.michael.vinh2@gmail.com>
2022-11-19 11:41:29 -0500

    Add clarifying comments

--- src/main/kotlin/aoc2018/day3/Part1.kt
+++ src/main/kotlin/aoc2018/day3/Part1.kt
@@ -3,8 +3,10 @@ package aoc2018.day3
 import dev.johnvinh.getInput
 
 fun main() {
+    // Claim ID, inches from left, inches from top, width, height
     val regex = Regex("^#([0-9]+?) @ ([0-9]+?),([0-9]+?): ([0-9]+?)x([0-9]+?)$")
     val input = getInput()
+    // 1000*1000 array representing number the number of claims that claim each inch
     var fabric = Array(1000) {Array(1000) {0} }
     for (line in input) {
         val match = regex.matchEntire(line)
