d94fa52b4b0d2aadcf5963490279d6d6951a01bf
John Vinh <john.michael.vinh2@gmail.com>
2022-11-19 11:30:06 -0500

    Create regex and match regex against each line

--- src/main/kotlin/aoc2018/day3/Part1.kt
+++ src/main/kotlin/aoc2018/day3/Part1.kt
@@ -3,5 +3,13 @@ package aoc2018.day3
 import dev.johnvinh.getInput
 
 fun main() {
+    val regex = Regex("^#([0-9]+?) @ ([0-9]+?),([0-9]+?): ([0-9]+?)x([0-9]+?)$")
     val input = getInput()
+    var fabric = Array(1000) {Array(1000) {0} }
+    for (line in input) {
+        val match = regex.matchEntire(line)
+        if (match != null) {
+            
+        }
+    }
 }
