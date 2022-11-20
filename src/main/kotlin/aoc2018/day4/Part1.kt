476fb65646d4f614fb38a67450c9527ef4fb9331
John Vinh <john.michael.vinh2@gmail.com>
2022-11-20 09:51:26 -0500

    Create regexes for parsing input

--- src/main/kotlin/aoc2018/day4/Part1.kt
+++ src/main/kotlin/aoc2018/day4/Part1.kt
@@ -7,4 +7,8 @@ class LogEntry(val month: Int, val year: Int, val hour: Int, val minute: Int)
 fun main() {
     val lines = getInput()
     val logEntries = ArrayList<LogEntry>()
+    val timeRegex = Regex("^\\[1518-([0-9]+?)-([0-9]+?) ([0-9]+?):([0-9]+?)]")
+    val shiftBeginRegex = Regex("Guard #([0-9]+?) begins shift$")
+    val fallsAsleepRegex = Regex("falls asleep$")
+    val wakesUpRegex = Regex("wakes up$")
 }
