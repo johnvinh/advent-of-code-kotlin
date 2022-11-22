48562f97d5d85f67daba47600fa1e2a8fef0456d
John Vinh <john.michael.vinh2@gmail.com>
2022-11-22 13:19:42 -0500

    Solve 2018 day 5 part 1

--- src/main/kotlin/aoc2018/day5/Part1.kt
+++ src/main/kotlin/aoc2018/day5/Part1.kt
@@ -4,7 +4,7 @@ import dev.johnvinh.getInput
 
 fun isReaction(first: Char, second: Char): Boolean {
     // First check if same letter
-    if (first.lowercase() != second.lowercase()) {
+    if (first.lowercaseChar() != second.lowercaseChar()) {
         return false
     } else if (first.isLowerCase() && second.isUpperCase()) {
         return true
@@ -15,17 +15,27 @@ fun isReaction(first: Char, second: Char): Boolean {
 }
 
 fun scanPolymer(polymer: String): String {
-    val sb = StringBuilder(polymer)
-    for (i in polymer.indices) {
-        if (i == polymer.length - 1) {
+    val polymerSb = StringBuilder(polymer)
+    var i = 0
+    while (true) {
+        if (i == polymerSb.length-1 || polymerSb.isEmpty()) {
             break
         }
-        val currentCharacter = polymer[i]
-        val nextCharacter = polymer[i + 1]
+        val currentCharacter = polymerSb[i]
+        val nextCharacter = polymerSb[i+1]
+        if (isReaction(currentCharacter, nextCharacter)) {
+            polymerSb.deleteCharAt(i)
+            polymerSb.deleteCharAt(i)
+            i = 0
+        } else {
+            i++
+        }
     }
-    return sb.toString()
+    return polymerSb.toString()
 }
 
 fun main() {
     val input = getInput()
+    val outputPolymer = scanPolymer(input[0])
+    println(outputPolymer.length)
 }
