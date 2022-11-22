7cd45b8498c358d18be71e3de2a9047ef59a395b
John Vinh <john.michael.vinh2@gmail.com>
2022-11-22 13:32:41 -0500

    Finish removeSpecificType function

--- src/main/kotlin/aoc2018/day5/Part2.kt
+++ src/main/kotlin/aoc2018/day5/Part2.kt
@@ -1,9 +1,30 @@
 package aoc2018.day5
 
+import dev.johnvinh.getInput
+import java.lang.IndexOutOfBoundsException
+
 fun removeSpecificType(type: Char, polymer: String): String {
-    return polymer
+    val polymerSb = StringBuilder(polymer)
+    var i = 0
+    while (true) {
+        if ( polymerSb.isEmpty()) {
+            break
+        }
+        var currentCharacter = '0'
+        try {
+            currentCharacter = polymerSb[i]
+        } catch (e: IndexOutOfBoundsException) {
+            break
+        }
+        if (currentCharacter.lowercaseChar() == type.lowercaseChar()) {
+            polymerSb.deleteCharAt(i)
+        }
+        i++
+    }
+    return polymerSb.toString()
 }
 
 fun main() {
-
+    val input = getInput()
+    println(removeSpecificType('B', input[0]))
 }
