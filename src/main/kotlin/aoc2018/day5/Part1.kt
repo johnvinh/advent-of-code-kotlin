4adbbd58d8a6af07008db64577789a4407fb1e84
John Vinh <john.michael.vinh2@gmail.com>
2022-11-22 11:01:19 -0500

    Finish checkIfReaction function

--- src/main/kotlin/aoc2018/day5/Part1.kt
+++ src/main/kotlin/aoc2018/day5/Part1.kt
@@ -3,6 +3,14 @@ package aoc2018.day5
 import dev.johnvinh.getInput
 
 fun checkIfReaction(first: Char, second: Char): Boolean {
+    // First check if same letter
+    if (first.lowercase() != second.lowercase()) {
+        return false
+    } else if (first.isLowerCase() && second.isUpperCase()) {
+        return true
+    } else if (first.isUpperCase() && second.isLowerCase()) {
+        return true
+    }
     return false
 }
 
