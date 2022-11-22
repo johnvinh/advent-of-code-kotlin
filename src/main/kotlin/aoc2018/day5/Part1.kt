b2ea820d30ab1f017c0729fc36c42738136828d8
John Vinh <john.michael.vinh2@gmail.com>
2022-11-22 11:06:34 -0500

    Rename checkIfReaction to isReaction

--- src/main/kotlin/aoc2018/day5/Part1.kt
+++ src/main/kotlin/aoc2018/day5/Part1.kt
@@ -2,7 +2,7 @@ package aoc2018.day5
 
 import dev.johnvinh.getInput
 
-fun checkIfReaction(first: Char, second: Char): Boolean {
+fun isReaction(first: Char, second: Char): Boolean {
     // First check if same letter
     if (first.lowercase() != second.lowercase()) {
         return false
