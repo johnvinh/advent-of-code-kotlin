79e69ab15910ee67c57f95cea26a3be621050257
John Vinh <john.michael.vinh2@gmail.com>
2022-11-23 10:17:56 -0500

    Start createGrid function

--- src/main/kotlin/aoc2018/day6/part1.kt
+++ src/main/kotlin/aoc2018/day6/part1.kt
@@ -2,6 +2,10 @@ package aoc2018.day6
 
 import dev.johnvinh.getInput
 
+fun createGrid(lines: String): Array<Array<Int>> {
+    return Array(1) {Array(1){0} }
+}
+
 fun main() {
     val input = getInput()
 }
