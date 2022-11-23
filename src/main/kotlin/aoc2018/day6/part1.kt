5338a4f4b8637776a25ca7c71d0436688ced5bbe
John Vinh <john.michael.vinh2@gmail.com>
2022-11-23 10:34:54 -0500

    Progress on createGrid function

--- src/main/kotlin/aoc2018/day6/part1.kt
+++ src/main/kotlin/aoc2018/day6/part1.kt
@@ -2,8 +2,18 @@ package aoc2018.day6
 
 import dev.johnvinh.getInput
 
-fun createGrid(lines: String): Array<Array<Int>> {
-    return Array(1) {Array(1){0} }
+fun createGrid(lines: List<String>): Array<IntArray> {
+    val regex = Regex("^([0-9]+?), ([0-9]+?)$")
+    val grid = Array(1000) { IntArray(1000) }
+    for ((i, line) in lines.withIndex()) {
+        val match = regex.matchEntire(line)
+        if (match != null) {
+            val column = match.groups[1]?.value?.toInt() ?: -1
+            val row = match.groups[2]?.value?.toInt() ?: -1
+            grid[column][row] = i
+        }
+    }
+    return grid
 }
 
 fun main() {
