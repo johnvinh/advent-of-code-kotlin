840614ce9e81c2e4ef18f96d557fc35d6cc686f3
John Vinh <john.michael.vinh2@gmail.com>
2022-11-25 10:22:57 -0500

    Alter move function header

--- src/main/kotlin/aoc2016/day1/day1.kt
+++ src/main/kotlin/aoc2016/day1/day1.kt
@@ -2,7 +2,7 @@ package aoc2016.day1
 
 import dev.johnvinh.getInput
 
-fun move(currentX: Int, currentY: Int, direction: Char): IntArray {
+fun move(currentX: Int, currentY: Int, newDirection: Char, previousDirection: Char): IntArray {
     return intArrayOf(currentX, currentY)
 }
 
