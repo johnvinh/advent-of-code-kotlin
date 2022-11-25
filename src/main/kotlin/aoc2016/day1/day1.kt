e7cace3621d31335d626897fdd1774769e685afd
John Vinh <john.michael.vinh2@gmail.com>
2022-11-25 10:20:57 -0500

    Start move function

--- src/main/kotlin/aoc2016/day1/day1.kt
+++ src/main/kotlin/aoc2016/day1/day1.kt
@@ -2,6 +2,10 @@ package aoc2016.day1
 
 import dev.johnvinh.getInput
 
+fun move(currentX: Int, currentY: Int, direction: Char): IntArray {
+    return intArrayOf(currentX, currentY)
+}
+
 fun main() {
     val lines = getInput()
 }
