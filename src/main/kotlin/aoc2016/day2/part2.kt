bd9d6f57582e8c3bfd66bef2f22d5610321eb000
John Vinh <john.michael.vinh2@gmail.com>
2022-11-29 21:19:26 -0500

    Initialize the new keypad

--- src/main/kotlin/aoc2016/day2/part2.kt
+++ src/main/kotlin/aoc2016/day2/part2.kt
@@ -4,4 +4,18 @@ import dev.johnvinh.getInput
 
 fun main() {
     val lines = getInput()
+    val buttons = Array(5) {Array<String>(5) {""} }
+    buttons[0][2] = "1"
+    buttons[1][1] = "2"
+    buttons[1][2] = "3"
+    buttons[1][3] = "4"
+    buttons[2][0] = "5"
+    buttons[2][1] = "6"
+    buttons[2][2] = "7"
+    buttons[2][3] = "8"
+    buttons[2][4] = "9"
+    buttons[3][1] = "A"
+    buttons[3][2] = "B"
+    buttons[3][3] = "C"
+    buttons[4][2] = "D"
 }
