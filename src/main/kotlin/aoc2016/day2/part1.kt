6f1458321d95ada4da1354ddd4ccbbc39a017761
John Vinh <john.michael.vinh2@gmail.com>
2022-11-29 20:30:29 -0500

    Initialize array representing the keypad buttons

--- src/main/kotlin/aoc2016/day2/part1.kt
+++ src/main/kotlin/aoc2016/day2/part1.kt
@@ -4,4 +4,12 @@ import dev.johnvinh.getInput
 
 fun main() {
     val lines = getInput()
+    val buttons = Array(3){IntArray(3) {0} }
+    var current = 1
+    for (i in buttons.indices) {
+        for (j in buttons[i].indices) {
+            buttons[i][j] = current
+            current++
+        }
+    }
 }
