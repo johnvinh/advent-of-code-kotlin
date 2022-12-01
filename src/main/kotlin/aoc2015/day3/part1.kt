c15f39ea94638833affa0b48b8c3521bae2bcf9a
John Vinh <john.michael.vinh2@gmail.com>
2022-11-30 21:25:29 -0500

    Solve 2015 day 3 part 1

--- src/main/kotlin/aoc2015/day3/part1.kt
+++ src/main/kotlin/aoc2015/day3/part1.kt
@@ -4,4 +4,32 @@ import dev.johnvinh.getInput
 
 fun main() {
     val lines = getInput()
+    val housesVisited = HashMap<Int, HashSet<Int>>()
+    var numHousesVisited = 1
+    var x = 0
+    var y = 0
+    val input = lines[0]
+
+    for (direction in input) {
+        when (direction) {
+            '^' -> y += 1
+            'v' -> y -= 1
+            '>' -> x += 1
+            '<' -> x -= 1
+        }
+        if (housesVisited.containsKey(x)) {
+            val houseX = housesVisited[x]
+            if (houseX != null) {
+                if (!houseX.contains(y)) {
+                    houseX.add(y)
+                    numHousesVisited++
+                }
+            }
+        } else {
+            housesVisited[x] = HashSet()
+            housesVisited[x]?.add(y)
+            numHousesVisited++
+        }
+    }
+    println(numHousesVisited)
 }
