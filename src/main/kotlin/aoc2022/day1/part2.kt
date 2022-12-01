c5dd4478acf123f66cd3bed166e785f21eb5cce4
John Vinh <john.michael.vinh2@gmail.com>
2022-12-01 11:10:03 -0500

    Solve 2022 day 1 part 2

--- src/main/kotlin/aoc2022/day1/part2.kt
+++ src/main/kotlin/aoc2022/day1/part2.kt
@@ -1,2 +1,10 @@
 package aoc2022.day1
 
+import dev.johnvinh.getInput
+
+fun main() {
+    val lines = getInput()
+    val caloriesPerElf = getCaloriesPerElf(lines)
+    caloriesPerElf.sortDescending()
+    println(caloriesPerElf[0] + caloriesPerElf[1] + caloriesPerElf[2])
+}
