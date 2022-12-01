d6066d2c9adec1a6db802529f7f2af72a294aa20
John Vinh <john.michael.vinh2@gmail.com>
2022-12-01 11:05:13 -0500

    Create getCaloriesPerElf function
    The getCaloriesPerElf function returns the sum of all the calories for each elf.

--- src/main/kotlin/aoc2022/day1/part1.kt
+++ src/main/kotlin/aoc2022/day1/part1.kt
@@ -2,6 +2,28 @@ package aoc2022.day1
 
 import dev.johnvinh.getInput
 
+fun getCaloriesPerElf(lines: List<String>): ArrayList<Int> {
+    val caloriesPerElf = ArrayList<Int>()
+    var currentCalories = 0
+    for (i in lines.indices) {
+        if (i == lines.size - 1) {
+            currentCalories += lines[i].toInt()
+            caloriesPerElf.add(currentCalories)
+        } else if (lines[i] == "") {
+            caloriesPerElf.add(currentCalories)
+            currentCalories = 0
+            continue
+        }
+        try {
+            val calories = lines[i].toInt()
+            currentCalories += calories
+        } catch (e: NumberFormatException) {
+            throw IllegalArgumentException("Bad input")
+        }
+    }
+    return caloriesPerElf
+}
+
 fun main() {
     val lines = getInput()
 }
