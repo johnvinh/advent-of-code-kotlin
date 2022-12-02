aa77444e8504c2df8e59f0d633d20acbdd551caa
John Vinh <john.michael.vinh2@gmail.com>
2022-12-02 10:39:05 -0500

    Finish calculateScore function

--- src/main/kotlin/aoc2022/day2/part1.kt
+++ src/main/kotlin/aoc2022/day2/part1.kt
@@ -3,7 +3,40 @@ package aoc2022.day2
 import dev.johnvinh.getInput
 
 fun calculateScore(opponentPlay: Char, myPlay: Char): Int {
-    return 0
+    var score = 0
+    when (myPlay) {
+        // Rock
+        'X' -> {
+            score += 1
+            when (opponentPlay) {
+                // Opponent rock
+                'A' -> score += 3
+                // Opponent scissors
+                'C' -> score += 6
+            }
+        }
+        // Paper
+        'Y' -> {
+            score += 2
+            when (opponentPlay) {
+                // Opponent rock
+                'A' -> score += 6
+                // Opponent paper
+                'B' -> score += 3
+            }
+        }
+        // Scissors
+        'Z' -> {
+            score += 3
+            when (opponentPlay) {
+                // Opponent paper
+                'B' -> score += 6
+                // Opponent scissors
+                'C' -> score += 3
+            }
+        }
+    }
+    return score
 }
 
 fun main() {
