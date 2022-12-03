727016182ffe21050aec5a19eaf26b055fce355b
John Vinh <john.michael.vinh2@gmail.com>
2022-12-03 10:58:32 -0500

    Finish getCommonItem function

--- src/main/kotlin/aoc2022/day3/part1.kt
+++ src/main/kotlin/aoc2022/day3/part1.kt
@@ -3,9 +3,25 @@ package aoc2022.day3
 import dev.johnvinh.getInput
 
 fun getCommonItem(rucksack: String): Char {
+    val firstCompartmentEnd = (rucksack.length / 2) - 1;
+    val firstCompartmentItems = HashSet<Char>()
+    for (i in rucksack.indices) {
+        // First half
+        if (i <= firstCompartmentEnd) {
+            firstCompartmentItems.add(rucksack[i])
+            continue
+        } else { // Second half
+            if (firstCompartmentItems.contains(rucksack[i])) {
+                return rucksack[i]
+            }
+        }
+    }
     return '0'
 }
 
 fun main() {
     val lines = getInput()
+    for (line in lines) {
+        println(getCommonItem(line))
+    }
 }
