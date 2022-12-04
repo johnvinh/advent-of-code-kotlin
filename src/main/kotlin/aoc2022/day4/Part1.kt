a24fc80ed300dc3eae26a95b855890a8916e3aff
John Vinh <john.michael.vinh2@gmail.com>
2022-12-04 09:50:11 -0500

    Start fullyContainsOtherSection function

--- src/main/kotlin/aoc2022/day4/Part1.kt
+++ src/main/kotlin/aoc2022/day4/Part1.kt
@@ -1,2 +1,11 @@
 package aoc2022.day4
 
+import dev.johnvinh.getInput
+
+fun fullyContainsOtherSection(sectionPair: String): Boolean {
+    return false
+}
+
+fun main() {
+    val lines = getInput()
+}
