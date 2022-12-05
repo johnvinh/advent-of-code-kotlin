e3ac6c178dd029b2c0e572bad647e32016b52fd8
John Vinh <john.michael.vinh2@gmail.com>
2022-12-05 15:35:39 -0500

    Find the stack declaration line

--- src/main/kotlin/aoc2022/day5/Part1.kt
+++ src/main/kotlin/aoc2022/day5/Part1.kt
@@ -1,11 +1,22 @@
 package aoc2022.day5
 
+import dev.johnvinh.getInput
 import java.util.Stack
 
-fun initializeStacks(): ArrayList<Stack<Char>> {
-    return ArrayList<Stack<Char>>()
+fun initializeStacks(input: List<String>): ArrayList<Stack<Char>> {
+    // Find which line the stack declaration numbers begin
+    var stackDeclarationIndice = 0
+    for (i in input.indices) {
+        if (input[i][1] == '1') {
+            stackDeclarationIndice = i
+            break
+        }
+    }
+    println(stackDeclarationIndice)
+    return ArrayList()
 }
 
 fun main() {
-
+    val lines = getInput()
+    initializeStacks(lines)
 }
