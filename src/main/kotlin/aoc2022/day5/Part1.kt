6ec0e25ad332b02167e2a3dfa27c7aa56c1378b8
John Vinh <john.michael.vinh2@gmail.com>
2022-12-05 16:13:52 -0500

    Finish executeInstructions function

--- src/main/kotlin/aoc2022/day5/Part1.kt
+++ src/main/kotlin/aoc2022/day5/Part1.kt
@@ -39,11 +39,36 @@ fun initializeStacks(input: List<String>): ArrayList<Stack<Char>> {
 }
 
 fun executeInstructions(input: List<String>, stacks: ArrayList<Stack<Char>>): ArrayList<Stack<Char>> {
+    var firstInstructionIndice = 0
+    // Find the line where the instructions start
+    for (i in input.indices) {
+        val line = input[i]
+        if (line.startsWith("move")) {
+            firstInstructionIndice = i
+            break
+        }
+    }
+    // Start executing the instructions
+    val regex = Regex("^move ([0-9]+?) from ([0-9]+?) to ([0-9]+)$")
+    for (i in firstInstructionIndice until input.size) {
+        val line = input[i]
+        val match = regex.matchEntire(line)
+        val amount = match?.groups?.get(1)?.value?.toInt() ?: 0
+        val from = match?.groups?.get(2)?.value?.toInt() ?: 0
+        val to = match?.groups?.get(3)?.value?.toInt() ?: 0
+        println("Amount: $amount, From: $from, To: $to")
+        // Move one at a time
+        for (j in 1..amount) {
+            val item = stacks[from - 1].pop()
+            stacks[to - 1].push(item)
+        }
+    }
     return ArrayList()
 }
 
 fun main() {
     val lines = getInput()
     val stacks = initializeStacks(lines)
+    executeInstructions(lines, stacks)
     println(stacks)
 }
