014d43bb118dfd3ee506431018a3947cd726298d
John Vinh <john.michael.vinh2@gmail.com>
2022-12-06 11:05:55 -0500

    Fix getStartOfMessageMarker test for first input

--- src/main/kotlin/aoc2022/day6/Part2.kt
+++ src/main/kotlin/aoc2022/day6/Part2.kt
@@ -1,5 +1,18 @@
 package aoc2022.day6
 
 fun getStartOfMessageMarker(input: String): Int {
+    for (i in input.indices) {
+        // For each character, check if this charavter and the 13 afterwards
+        // are all unique with a set
+        val seenCharacters = HashSet<Char>()
+        for (j in i..i+13) {
+            seenCharacters.add(input[j])
+        }
+        // If no duplicates were added, we know this was the start of the marker
+        val numCharacters = (i + 13) + 1
+        if (seenCharacters.size == 14) {
+            return numCharacters
+        }
+    }
     return -1
 }
