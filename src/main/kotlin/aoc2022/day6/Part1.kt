30c74f4427e96aff35cf61de932b3fb4e73d98f1
John Vinh <john.michael.vinh2@gmail.com>
2022-12-06 10:55:24 -0500

    Fix getEndOfMarkerCharacter test for first test input

--- src/main/kotlin/aoc2022/day6/Part1.kt
+++ src/main/kotlin/aoc2022/day6/Part1.kt
@@ -1,2 +1,19 @@
 package aoc2022.day6
 
+fun getEndOfMarkerCharacter(input: String): Int {
+    for (i in input.indices) {
+        // For each character, check if this charavter and the three afterwards
+        // are all unique with a set
+        val seenCharacters = HashSet<Char>()
+        seenCharacters.add(input[i])
+        seenCharacters.add(input[i+1])
+        seenCharacters.add(input[i+2])
+        seenCharacters.add(input[i+3])
+        // If no duplicates were added, we know this was the start of the marker
+        val numCharacters = (i + 3) + 1
+        if (seenCharacters.size == 4) {
+            return numCharacters
+        }
+    }
+    return -1
+}
--- src/test/kotlin/aoc2022/day6/Part1KtTest.kt
+++ src/test/kotlin/aoc2022/day6/Part1KtTest.kt
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Assertions.*
 class Part1KtTest {
 
     @Test
-    fun getEndOfMarkerCharacter() {
+    fun getEndOfMarkerCharacterTest() {
+        assertEquals(7, getEndOfMarkerCharacter("mjqjpqmgbljsphdztnvjfqwrcgsmlb"))
     }
 }
