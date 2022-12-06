5f0375d55b8b9698ad7f4c087f6a165d85748d96
John Vinh <john.michael.vinh2@gmail.com>
2022-12-06 11:13:11 -0500

    Remove magic numbers

--- src/main/kotlin/aoc2022/day6/Part2.kt
+++ src/main/kotlin/aoc2022/day6/Part2.kt
@@ -2,17 +2,22 @@ package aoc2022.day6
 
 import dev.johnvinh.getInput
 
+object ProblemConstants {
+    const val NUM_ADDITIONAL_CHARACTERS = 13
+    const val NUM_CHARACTERS = 14
+}
+
 fun getStartOfMessageMarker(input: String): Int {
     for (i in input.indices) {
         // For each character, check if this charavter and the 13 afterwards
         // are all unique with a set
         val seenCharacters = HashSet<Char>()
-        for (j in i..i+13) {
+        for (j in i..i+ProblemConstants.NUM_ADDITIONAL_CHARACTERS) {
             seenCharacters.add(input[j])
         }
         // If no duplicates were added, we know this was the start of the marker
-        val numCharacters = (i + 13) + 1
-        if (seenCharacters.size == 14) {
+        val numCharacters = (i + ProblemConstants.NUM_ADDITIONAL_CHARACTERS) + 1
+        if (seenCharacters.size == ProblemConstants.NUM_CHARACTERS) {
             return numCharacters
         }
     }
