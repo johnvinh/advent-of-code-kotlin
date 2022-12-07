760b126ddf135ed75db20a431d5153a6a101c6e6
John Vinh <john.michael.vinh2@gmail.com>
2022-12-07 15:17:04 -0500

    Add problem constants

--- src/main/kotlin/aoc2022/day7/Part2.kt
+++ src/main/kotlin/aoc2022/day7/Part2.kt
@@ -1,2 +1,15 @@
 package aoc2022.day7
 
+import dev.johnvinh.getInput
+
+object ProblemConsants {
+    const val TOTAL_DISK_SPACE = 70000000
+    const val SPACE_REQUIRED = 30000000
+}
+
+fun main() {
+    val input = getInput()
+    val root = constructDirTree(input)
+    val childDirs = getChildDirs(root)
+
+}
