6cf7066943ecbe736edb814618954e9775a12ad3
John Vinh <john.michael.vinh2@gmail.com>
2022-12-08 09:31:48 -0500

    Change tests to convert input to 2d array

--- src/test/kotlin/aoc2022/day8/Part1KtTest.kt
+++ src/test/kotlin/aoc2022/day8/Part1KtTest.kt
@@ -4,9 +4,12 @@ import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.TestInstance
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class Part1KtTest {
     lateinit var input: List<String>
+    lateinit var grid: Array<IntArray>
 
     @BeforeAll
     fun initializeInput() {
@@ -15,13 +18,24 @@ class Part1KtTest {
                 "65332" +
                 "33549" +
                 "35390")
+        val columns = input[0].length
+        val rows = input.size
+        grid = Array(rows){IntArray(columns)}
+        for (i in input.indices) {
+            for (j in input[i].indices) {
+                val num = (input[i][j]).toString().toInt()
+                grid[i][j] = num
+            }
+        }
     }
 
     @Test
-    fun isVisible() {
+    fun isVisible_topLeft_True() {
+        assertTrue(isVisible(grid, 1, 1))
     }
 
     @Test
-    fun getNumVisible() {
+    fun getNumVisible_21() {
+        assertEquals(21, getNumVisible(grid))
     }
 }
