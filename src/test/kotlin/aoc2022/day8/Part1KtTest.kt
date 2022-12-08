338f13173375b7b87b36f0d4ab920d317483b5ba
John Vinh <john.michael.vinh2@gmail.com>
2022-12-08 09:17:26 -0500

    Add initialization function to tests

--- src/test/kotlin/aoc2022/day8/Part1KtTest.kt
+++ src/test/kotlin/aoc2022/day8/Part1KtTest.kt
@@ -3,8 +3,19 @@ package aoc2022.day8
 import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeAll
 
 class Part1KtTest {
+    lateinit var input: List<String>
+
+    @BeforeAll
+    fun initializeInput() {
+        input = arrayListOf("30373" +
+                "25512" +
+                "65332" +
+                "33549" +
+                "35390")
+    }
 
     @Test
     fun isVisible() {
