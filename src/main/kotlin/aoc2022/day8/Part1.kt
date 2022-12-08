4f75bc8678ae73eda1e69412db34ff70dd5e88bd
John Vinh <john.michael.vinh2@gmail.com>
2022-12-08 10:36:14 -0500

    Solve 2022 day 8 part 1

--- src/main/kotlin/aoc2022/day8/Part1.kt
+++ src/main/kotlin/aoc2022/day8/Part1.kt
@@ -1,11 +1,70 @@
 package aoc2022.day8
 
+import dev.johnvinh.getInput
+
 fun isVisible(grid: Array<IntArray>, row: Int, col: Int): Boolean {
+    val thisNumber = grid[row][col]
+    // Top Edge
+    if (row == 0) {
+        return true
+    } else if (col == 0) { // Left Edge
+        return true
+    } else if (row == grid.size-1) { // Bottom Edge
+        return true
+    } else if (col == grid[0].size-1) { // Right Edge
+        return true
+    }
+
+    var visibleFromTop = true
+    var visibleFromBottom = true
+    var visibleFromLeft = true
+    var visibleFromRight = true
+
+    // Visible from top
+    for (i in 0 until row) {
+        if (grid[i][col] >= thisNumber) {
+            visibleFromTop = false
+            break
+        }
+    }
+    // Visible from bottom
+    for (i in row+1 until grid.size) {
+        if (grid[i][col] >= thisNumber) {
+            visibleFromBottom = false
+            break
+        }
+    }
+    // Visible from left
+    for (i in 0 until col) {
+        if (grid[row][i] >= thisNumber) {
+            visibleFromLeft = false
+            break
+        }
+    }
+    // Visible from right
+    for (i in col+1 until grid[0].size) {
+        if (grid[row][i] >= thisNumber) {
+            visibleFromRight = false
+            break
+        }
+    }
+    // It's considered visible if it's visible from any side
+    if (visibleFromTop || visibleFromBottom || visibleFromRight || visibleFromLeft) {
+        return true
+    }
     return false
 }
 
 fun getNumVisible(grid: Array<IntArray>): Int {
-    return 0
+    var numVisible = 0
+    for (i in grid.indices) {
+        for (j in grid[0].indices) {
+            if (isVisible(grid, i, j)) {
+                numVisible++
+            }
+        }
+    }
+    return numVisible
 }
 
 fun convertToIntArray(lines: List<String>): Array<IntArray> {
@@ -19,4 +78,10 @@ fun convertToIntArray(lines: List<String>): Array<IntArray> {
         }
     }
     return grid
+}
+
+fun main() {
+    val lines = getInput()
+    val grid = convertToIntArray(lines)
+    println(getNumVisible(grid))
 }
