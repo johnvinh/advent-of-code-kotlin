980321afe3d934a23eadde4dd2fb7082189064cf
John Vinh <john.michael.vinh2@gmail.com>
2022-12-08 09:33:22 -0500

    Create convertToIntArray method

--- src/main/kotlin/aoc2022/day8/Part1.kt
+++ src/main/kotlin/aoc2022/day8/Part1.kt
@@ -1,9 +1,22 @@
 package aoc2022.day8
 
-fun isVisible(lines: List<String>, row: Int, col: Int): Boolean {
+fun isVisible(grid: Array<IntArray>, row: Int, col: Int): Boolean {
     return false
 }
 
-fun getNumVisible(lines: List<String>): Int {
+fun getNumVisible(grid: Array<IntArray>): Int {
     return 0
+}
+
+fun convertToIntArray(lines: List<String>): Array<IntArray> {
+    val columns = lines[0].length
+    val rows = lines.size
+    val grid = Array(rows){IntArray(columns)}
+    for (i in lines.indices) {
+        for (j in lines[i].indices) {
+            val num = (lines[i][j]).toString().toInt()
+            grid[i][j] = num
+        }
+    }
+    return grid
 }
