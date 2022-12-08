0fc459ba0c7db18507764943c28d40b48e4cc47c
John Vinh <john.michael.vinh2@gmail.com>
2022-12-08 09:10:42 -0500

    Start unit tests for 2022 day 8 part 1

---
+++ src/test/kotlin/aoc2022/day8/Part1KtTest.kt
@@ -0,0 +1,16 @@
+package aoc2022.day8
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class Part1KtTest {
+
+    @Test
+    fun isVisible() {
+    }
+
+    @Test
+    fun getNumVisible() {
+    }
+}
