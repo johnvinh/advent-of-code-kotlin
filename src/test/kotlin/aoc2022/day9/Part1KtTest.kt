d8c4bf843f2e0c702657b2830e91e10d5650019b
John Vinh <john.michael.vinh2@gmail.com>
2022-12-09 11:19:01 -0500

    Add test input

--- src/test/kotlin/aoc2022/day9/Part1KtTest.kt
+++ src/test/kotlin/aoc2022/day9/Part1KtTest.kt
@@ -3,8 +3,24 @@ package aoc2022.day9
 import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
+import kotlin.test.BeforeTest
 
 class Part1KtTest {
+    private var input = listOf(
+        "R 4",
+        "U 4",
+        "L 3",
+        "D 1",
+        "R 4",
+        "D 1",
+        "L 5",
+        "R 2"
+    )
+
+    @BeforeTest
+    fun init() {
+
+    }
 
     @Test
     fun moveHead() {
