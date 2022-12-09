323f711cfd16911deb7cff355eb2de30340c047b
John Vinh <john.michael.vinh2@gmail.com>
2022-12-09 11:16:16 -0500

    Start 2022 day 9 part 1 and tests

---
+++ src/main/kotlin/aoc2022/day9/Part1.kt
@@ -0,0 +1,10 @@
+package aoc2022.day9
+
+/**
+ * Move the head to a new position.
+ * Returns the grid after the movements
+ * are made
+ */
+fun moveHead(direction: Char, magnitude: Int): Array<IntArray> {
+    return Array<IntArray>(0){ IntArray(0) }
+}
