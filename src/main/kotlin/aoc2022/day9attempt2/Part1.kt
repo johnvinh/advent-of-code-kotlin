a5ee6b2ece1013c54f2a862a9a0f8eccb47f698b
John Vinh <john.michael.vinh2@gmail.com>
2022-12-10 16:13:57 -0500

    Start moveTail method

--- src/main/kotlin/aoc2022/day9attempt2/Part1.kt
+++ src/main/kotlin/aoc2022/day9attempt2/Part1.kt
@@ -5,10 +5,14 @@ fun moveHead(headPos: ArrayList<Int>, direction: Char) {
     headPos[1] = if (direction == 'U') headPos[1] + 1 else if (direction == 'D') headPos[1] - 1 else headPos[1]
 }
 
+fun moveTail(headPos: ArrayList<Int>, tailPos: ArrayList<Int>) {
+    
+}
+
 fun moveInstruction(headPos: ArrayList<Int>, tailPos: ArrayList<Int>, direction: Char, magnitude: Int):
         ArrayList<ArrayList<Int>> {
     for (n in 1..magnitude) {
-
+        moveHead(headPos, direction)
     }
     return ArrayList()
 }
