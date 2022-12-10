dc04e65a1ad975d3830151771e00d1fa5c93a967
John Vinh <john.michael.vinh2@gmail.com>
2022-12-10 17:56:57 -0500

    Add kotlindoc to part 1

--- src/main/kotlin/aoc2022/day9attempt2/Part1.kt
+++ src/main/kotlin/aoc2022/day9attempt2/Part1.kt
@@ -3,14 +3,33 @@ package aoc2022.day9attempt2
 import dev.johnvinh.getInput
 import kotlin.math.abs
 
+/**
+ * Class for 2022 day 1 part 1.
+ * Used for easily tracking the positions
+ * the tail has been in.
+ */
 class Part1 {
+    /**
+     * A HashMap with an X value as the key and a Y value as
+     * the value.
+     */
     val tailPositions = HashMap<Int, HashSet<Int>>()
 
+    /**
+     * Move the head one unit.
+     * @param   headPos the x and y positions of the head
+     * @param   direction   a character ('U', 'D', 'R', 'L') determing the direction to move in
+     */
     fun moveHead(headPos: ArrayList<Int>, direction: Char) {
         headPos[0] = if (direction == 'R') headPos[0] + 1 else if (direction == 'L') headPos[0] - 1 else headPos[0]
         headPos[1] = if (direction == 'U') headPos[1] + 1 else if (direction == 'D') headPos[1] - 1 else headPos[1]
     }
 
+    /**
+     * Moves the tail based on the position of the head.
+     * @param   headPos the x and y positions of the head
+     * @param   tailPos     the x and y positions of the tail
+     */
     fun moveTail(headPos: ArrayList<Int>, tailPos: ArrayList<Int>) {
         val xDifference = headPos[0] - tailPos[0]
         val yDifference = headPos[1] - tailPos[1]
@@ -45,6 +64,13 @@ class Part1 {
         }
     }
 
+    /**
+     * Moves the head and the tail if it needs to be moved.
+     * @param   headPos the x and y positions of the head
+     * @param   tailPos the x and y positions of the tail
+     * @param   direction   a single char ('U', 'D', 'R', 'L') determining the direction to move
+     * @param   magnitude   the number of units to move in the direction
+     */
     fun moveInstruction(headPos: ArrayList<Int>, tailPos: ArrayList<Int>, direction: Char, magnitude: Int):
             ArrayList<ArrayList<Int>> {
 
@@ -61,6 +87,10 @@ class Part1 {
         return arrayListOf(headPos, tailPos)
     }
 
+    /**
+     * Executes a list of instructions.
+     * @param   lines   the list of instructions
+     */
     fun executeInstructions(lines: List<String>) {
         val head = arrayListOf(0, 0)
         val tail = arrayListOf(0, 0)
@@ -72,6 +102,10 @@ class Part1 {
         }
     }
 
+    /**
+     * Gets the number of unique positions the tail has visited.
+     * @return  the number of positions visited by the tail
+     */
     fun getNumTailPositions(): Int {
         var numPositions = 0
         for (xValue in tailPositions.keys) {
@@ -81,6 +115,9 @@ class Part1 {
     }
 }
 
+/**
+ * Run Part 1.
+ */
 fun main() {
     val input = getInput()
     val part1Obj = Part1()
