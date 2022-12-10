1aef209f86097c40b703dd89b065d90aaf43b254
John Vinh <john.michael.vinh2@gmail.com>
2022-12-10 17:24:42 -0500

    Add executeInstructions and getNumTailPositions methods

--- src/main/kotlin/aoc2022/day9attempt2/Part1.kt
+++ src/main/kotlin/aoc2022/day9attempt2/Part1.kt
@@ -2,51 +2,81 @@ package aoc2022.day9attempt2
 
 import kotlin.math.abs
 
-fun moveHead(headPos: ArrayList<Int>, direction: Char) {
-    headPos[0] = if (direction == 'R') headPos[0] + 1 else if (direction == 'L') headPos[0] - 1 else headPos[0]
-    headPos[1] = if (direction == 'U') headPos[1] + 1 else if (direction == 'D') headPos[1] - 1 else headPos[1]
-}
+class Part1 {
+    val tailPositions = HashMap<Int, HashSet<Int>>()
 
-fun moveTail(headPos: ArrayList<Int>, tailPos: ArrayList<Int>) {
-    val xDifference = headPos[0] - tailPos[0]
-    val yDifference = headPos[1] - tailPos[1]
-    // Not touching and not in same row or column
-    // Move diagonally
-    println("X Difference: $xDifference, Y Difference: $yDifference")
-    if ((abs(xDifference) == 2 || abs(yDifference) == 2) && (headPos[0] != tailPos[0] && headPos[1] != tailPos[1])) {
-        if (yDifference > 0 && xDifference > 0) {
-            tailPos[0]++
-            tailPos[1]++
-        } else if (yDifference > 0 && xDifference < 0) {
-            tailPos[0]--
-            tailPos[1]++
-        } else if (yDifference < 0 && xDifference > 0) {
-            tailPos[0]++
-            tailPos[1]--
-        } else if (yDifference < 0 && xDifference < 0) {
-            tailPos[0]--
-            tailPos[1]--
-        }
-    } else if (abs(xDifference) == 2) {
-        if (xDifference > 0) {
-            tailPos[0]++
-        } else {
-            tailPos[0]--
-        }
-    } else if (abs(yDifference) == 2) {
-        if (yDifference > 0) {
-            tailPos[1]++
-        } else {
-            tailPos[1]--
+    fun moveHead(headPos: ArrayList<Int>, direction: Char) {
+        headPos[0] = if (direction == 'R') headPos[0] + 1 else if (direction == 'L') headPos[0] - 1 else headPos[0]
+        headPos[1] = if (direction == 'U') headPos[1] + 1 else if (direction == 'D') headPos[1] - 1 else headPos[1]
+    }
+
+    fun moveTail(headPos: ArrayList<Int>, tailPos: ArrayList<Int>) {
+        val xDifference = headPos[0] - tailPos[0]
+        val yDifference = headPos[1] - tailPos[1]
+        // Not touching and not in same row or column
+        // Move diagonally
+        println("X Difference: $xDifference, Y Difference: $yDifference")
+        if ((abs(xDifference) == 2 || abs(yDifference) == 2) && (headPos[0] != tailPos[0] && headPos[1] != tailPos[1])) {
+            if (yDifference > 0 && xDifference > 0) {
+                tailPos[0]++
+                tailPos[1]++
+            } else if (yDifference > 0 && xDifference < 0) {
+                tailPos[0]--
+                tailPos[1]++
+            } else if (yDifference < 0 && xDifference > 0) {
+                tailPos[0]++
+                tailPos[1]--
+            } else if (yDifference < 0 && xDifference < 0) {
+                tailPos[0]--
+                tailPos[1]--
+            }
+        } else if (abs(xDifference) == 2) {
+            if (xDifference > 0) {
+                tailPos[0]++
+            } else {
+                tailPos[0]--
+            }
+        } else if (abs(yDifference) == 2) {
+            if (yDifference > 0) {
+                tailPos[1]++
+            } else {
+                tailPos[1]--
+            }
         }
     }
-}
 
-fun moveInstruction(headPos: ArrayList<Int>, tailPos: ArrayList<Int>, direction: Char, magnitude: Int):
-        ArrayList<ArrayList<Int>> {
-    for (n in 1..magnitude) {
-        moveHead(headPos, direction)
-        moveTail(headPos, tailPos)
+    fun moveInstruction(headPos: ArrayList<Int>, tailPos: ArrayList<Int>, direction: Char, magnitude: Int):
+            ArrayList<ArrayList<Int>> {
+
+        for (n in 1..magnitude) {
+            moveHead(headPos, direction)
+            moveTail(headPos, tailPos)
+            if (tailPositions.containsKey(tailPos[0])) {
+                tailPositions[tailPos[0]]?.add(tailPos[1])
+            } else {
+                tailPositions[tailPos[0]] = HashSet()
+                tailPositions[tailPos[0]]?.add(tailPos[1])
+            }
+        }
+        return arrayListOf(headPos, tailPos)
     }
-    return arrayListOf(headPos, tailPos)
+
+    fun executeInstructions(lines: List<String>) {
+        val head = arrayListOf(0, 0)
+        val tail = arrayListOf(0, 0)
+        for (line in lines) {
+            val parts = line.split(" ")
+            val direction = parts[0][0]
+            val magnitude = parts[1].toInt()
+            moveInstruction(head, tail, direction, magnitude)
+        }
+    }
+
+    fun getNumTailPositions(): Int {
+        var numPositions = 0
+        for (xValue in tailPositions.keys) {
+            numPositions += tailPositions[xValue]?.size ?: 0
+        }
+        return numPositions
+    }
 }
--- src/test/kotlin/aoc2022/day9attempt2/Part1KtTest.kt
+++ src/test/kotlin/aoc2022/day9attempt2/Part1KtTest.kt
@@ -1,84 +1,110 @@
 package aoc2022.day9attempt2
 
+import aoc2022.day5.executeInstructions
 import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
+import kotlin.test.BeforeTest
 
 class Part1KtTest {
+    lateinit var part1: Part1
+    private val input = arrayListOf(
+        "R 4",
+        "U 4",
+        "L 3",
+        "D 1",
+        "R 4",
+        "D 1",
+        "L 5",
+        "R 2"
+    )
+
+    @BeforeTest
+    fun init() {
+        part1 = Part1()
+    }
 
     @Test
     fun moveInstruction_Origin_R4() {
         val expected = arrayListOf(arrayListOf(4, 0), arrayListOf(3, 0))
-        val actual = moveInstruction(arrayListOf(0, 0), arrayListOf(0, 0), 'R', 4)
+        val actual = part1.moveInstruction(arrayListOf(0, 0), arrayListOf(0, 0), 'R', 4)
         assertEquals(expected, actual)
     }
 
     @Test
     fun moveHead_Examples() {
         val head = arrayListOf(0, 0)
-        moveHead(head, 'R')
+        part1.moveHead(head, 'R')
         assertEquals(arrayListOf(1, 0), head)
-        moveHead(head, 'U')
+        part1.moveHead(head, 'U')
         assertEquals(arrayListOf(1, 1), head)
-        moveHead(head, 'L')
+        part1.moveHead(head, 'L')
         assertEquals(arrayListOf(0, 1), head)
-        moveHead(head, 'D')
+        part1.moveHead(head, 'D')
         assertEquals(arrayListOf(0, 0), head)
     }
 
     @Test
     fun moveInstruction_R4_head40_tail30() {
         val expected = arrayListOf(arrayListOf(4, 0), arrayListOf(3, 0))
-        val actual = moveInstruction(arrayListOf(0, 0), arrayListOf(0, 0), 'R', 4)
+        val actual = part1.moveInstruction(arrayListOf(0, 0), arrayListOf(0, 0), 'R', 4)
         assertEquals(expected, actual)
     }
 
     @Test
     fun moveInstruction_U4_head44_tail_43() {
         val expected = arrayListOf(arrayListOf(4, 4), arrayListOf(4, 3))
-        val actual = moveInstruction(arrayListOf(4, 0), arrayListOf(3, 0), 'U', 4)
+        val actual = part1.moveInstruction(arrayListOf(4, 0), arrayListOf(3, 0), 'U', 4)
         assertEquals(expected, actual)
     }
 
     @Test
     fun moveInstruction_L3_head14_tail24() {
         val expected = arrayListOf(arrayListOf(1, 4), arrayListOf(2, 4))
-        val actual = moveInstruction(arrayListOf(4, 4), arrayListOf(4, 3), 'L', 3)
+        val actual = part1.moveInstruction(arrayListOf(4, 4), arrayListOf(4, 3), 'L', 3)
         assertEquals(expected, actual)
     }
 
     @Test
     fun moveInstruction_D1_head13_tail24() {
         val expected = arrayListOf(arrayListOf(1, 3), arrayListOf(2, 4))
-        val actual = moveInstruction(arrayListOf(1, 4), arrayListOf(2, 4), 'D', 1)
+        val actual = part1.moveInstruction(arrayListOf(1, 4), arrayListOf(2, 4), 'D', 1)
         assertEquals(expected, actual)
     }
 
     @Test
     fun moveInstruction_R4_head53_tail43() {
         val expected = arrayListOf(arrayListOf(5, 3), arrayListOf(4, 3))
-        val actual = moveInstruction(arrayListOf(1, 3), arrayListOf(2, 4), 'R', 4)
+        val actual = part1.moveInstruction(arrayListOf(1, 3), arrayListOf(2, 4), 'R', 4)
         assertEquals(expected, actual)
     }
 
     @Test
     fun moveInstruction_D1_head52_tail43() {
         val expected = arrayListOf(arrayListOf(5, 2), arrayListOf(4, 3))
-        val actual = moveInstruction(arrayListOf(5, 3), arrayListOf(4, 3), 'D', 1)
+        val actual = part1.moveInstruction(arrayListOf(5, 3), arrayListOf(4, 3), 'D', 1)
         assertEquals(expected, actual)
     }
 
     @Test
     fun moveInstruction_L5_head02_tail12() {
         val expected = arrayListOf(arrayListOf(0, 2), arrayListOf(1, 2))
-        val actual = moveInstruction(arrayListOf(5, 2), arrayListOf(4, 3), 'L', 5)
+        val actual = part1.moveInstruction(arrayListOf(5, 2), arrayListOf(4, 3), 'L', 5)
         assertEquals(expected, actual)
     }
 
     @Test
     fun moveInstruction_R2_head22_tail12() {
         val expected = arrayListOf(arrayListOf(2, 2), arrayListOf(1, 2))
-        val actual = moveInstruction(arrayListOf(0, 2), arrayListOf(1, 2), 'R', 2)
+        val actual = part1.moveInstruction(arrayListOf(0, 2), arrayListOf(1, 2), 'R', 2)
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun numTailPositions_13() {
+        part1.executeInstructions(input)
+        val expected = 13
+        val actual = part1.getNumTailPositions()
         assertEquals(expected, actual)
     }
 }
