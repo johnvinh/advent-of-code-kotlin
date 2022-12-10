17eca64d64e0d914f7a3960ed2ded922b860c2fa
John Vinh <john.michael.vinh2@gmail.com>
2022-12-10 16:08:03 -0500

    Create moveHead method

--- src/main/kotlin/aoc2022/day9attempt2/Part1.kt
+++ src/main/kotlin/aoc2022/day9attempt2/Part1.kt
@@ -1,6 +1,14 @@
 package aoc2022.day9attempt2
 
+fun moveHead(headPos: ArrayList<Int>, direction: Char) {
+    headPos[0] = if (direction == 'R') headPos[0] + 1 else if (direction == 'L') headPos[0] - 1 else headPos[0]
+    headPos[1] = if (direction == 'U') headPos[1] + 1 else if (direction == 'D') headPos[1] - 1 else headPos[1]
+}
+
 fun moveInstruction(headPos: ArrayList<Int>, tailPos: ArrayList<Int>, direction: Char, magnitude: Int):
         ArrayList<ArrayList<Int>> {
+    for (n in 1..magnitude) {
+
+    }
     return ArrayList()
 }
