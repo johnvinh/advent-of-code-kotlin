15f604970b9b5fadb9520e1f51fe337c6ded0f6a
John Vinh <john.michael.vinh2@gmail.com>
2022-12-10 15:39:51 -0500

    Create moveInstruction function and add test

--- src/main/kotlin/aoc2022/day9attempt2/Part1.kt
+++ src/main/kotlin/aoc2022/day9attempt2/Part1.kt
@@ -1,2 +1,6 @@
 package aoc2022.day9attempt2
 
+fun moveInstruction(headPos: ArrayList<Int>, tailPos: ArrayList<Int>, direction: Char, magnitude: Int):
+        ArrayList<ArrayList<Int>> {
+    return ArrayList()
+}
---
+++ src/test/kotlin/aoc2022/day9attempt2/Part1KtTest.kt
@@ -0,0 +1,12 @@
+package aoc2022.day9attempt2
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class Part1KtTest {
+
+    @Test
+    fun moveInstruction() {
+    }
+}
