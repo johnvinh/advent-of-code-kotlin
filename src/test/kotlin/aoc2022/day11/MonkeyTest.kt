de84c77132d5429ee36d503b5f91c36e3018e64a
John Vinh <john.michael.vinh2@gmail.com>
2022-12-11 10:24:02 -0500

    Create test for getItemWorryIncrease

--- src/test/kotlin/aoc2022/day11/MonkeyTest.kt
+++ src/test/kotlin/aoc2022/day11/MonkeyTest.kt
@@ -1,5 +1,20 @@
 package aoc2022.day11
 
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+import kotlin.test.BeforeTest
 
-class MonkeyTest
+class MonkeyTest {
+    lateinit var monkey: Monkey
+
+    @BeforeTest
+    fun init() {
+        monkey = Monkey(arrayListOf(79, 98), "new = old * 19", "divisible by 23", "throw to monkey 2",
+            "throw to monkey 3")
+    }
+
+    @Test
+    fun getItemWorryIncrease_79_1501() {
+        assertEquals(1501, monkey.getItemWorryIncrease(monkey.items[0]))
+    }
+}
