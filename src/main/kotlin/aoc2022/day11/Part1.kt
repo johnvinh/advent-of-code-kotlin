24fe4d1fd82f843de9d7af98b95bf78db3ba40e1
John Vinh <john.michael.vinh2@gmail.com>
2022-12-11 11:14:50 -0500

    Finish createMonkeyList method

--- src/main/kotlin/aoc2022/day11/Part1.kt
+++ src/main/kotlin/aoc2022/day11/Part1.kt
@@ -1,5 +1,6 @@
 package aoc2022.day11
 
+import dev.johnvinh.getInput
 import kotlin.math.floor
 
 class Monkey(val items: ArrayList<Int>, private val operation: String,
@@ -45,5 +46,65 @@ class Monkey(val items: ArrayList<Int>, private val operation: String,
             return trueMonkey
         }
         return falseMonkey
+    }
+}
+
+fun createMonkeyList(input: List<String>): ArrayList<Monkey> {
+    val out = ArrayList<Monkey>()
+    var startingItems: String = ""
+    var startingItemsList: ArrayList<Int>
+    var operation: String = ""
+    var test: String = ""
+    var trueAction: String = ""
+    var falseAction: String = ""
+
+    for (i in input.indices) {
+        val line = input[i]
+        if (line == "" || i == (input.size - 1)) {
+            val startingItemParts = startingItems.split(", ")
+            startingItemsList = ArrayList()
+            for (part in startingItemParts) {
+                startingItemsList.add(part.toInt())
+            }
+            out.add(
+                Monkey(
+                    startingItemsList,
+                    operation,
+                    test,
+                    trueAction,
+                    falseAction
+                )
+            )
+        } else if (line.indexOf("Starting items") != -1) {
+            val startingItemsRegex = Regex("Starting items: (.+)$")
+            val match = startingItemsRegex.find(line)
+            startingItems = match?.groups?.get(1)?.value.toString()
+        } else if (line.indexOf("Operation") != -1) {
+            val operationRegex = Regex("Operation: (.+)$")
+            val match = operationRegex.find(line)
+            operation = match?.groups?.get(1)?.value.toString()
+        } else if (line.indexOf("Test") != -1) {
+            val testRegex = Regex("Test: (.+)$")
+            val match = testRegex.find(line)
+            test = match?.groups?.get(1)?.value.toString()
+        } else if (line.indexOf("If true") != -1) {
+            val ifTrueRegex = Regex("If true: (.+)$")
+            val match = ifTrueRegex.find(line)
+            trueAction = match?.groups?.get(1)?.value.toString()
+        } else if (line.indexOf("If false") != -1) {
+            val ifFalseRegex = Regex("If false: (.+)$")
+            val match = ifFalseRegex.find(line)
+            falseAction = match?.groups?.get(1)?.value.toString()
+        }
+    }
+    return out
+}
+
+fun main() {
+    val input = getInput()
+    val monkeys = createMonkeyList(input)
+    for (i in monkeys.indices) {
+        println("Monkey $i")
+        println("Starting items: ${monkeys[i].items}")
     }
 }
