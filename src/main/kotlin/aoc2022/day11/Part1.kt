b8dc53187bac94ae482e37812b79c2b4c9fe6805
John Vinh <john.michael.vinh2@gmail.com>
2022-12-11 10:18:52 -0500

    Create getItemWorryIncrease method for Monkey class

--- src/main/kotlin/aoc2022/day11/Part1.kt
+++ src/main/kotlin/aoc2022/day11/Part1.kt
@@ -1,6 +1,28 @@
 package aoc2022.day11
 
-class Monkey(val items: ArrayList<Int>, val operation: String,
-             val test: String, val trueOperation: String, val falseOperation: String) {
-
+class Monkey(val items: ArrayList<Int>, private val operation: String,
+             val test: String, val trueAction: String, val falseAction: String) {
+    fun getItemWorryIncrease(item: Int): Int {
+        val operationRegex = Regex("new = old ([+-]) (.+)$")
+        val match = operationRegex.find(operation)
+        val operator = match?.groups?.get(1)?.value
+        val operand = match?.groups?.get(2)?.value
+        var actualOperand = 0
+        when (operand) {
+            "old" -> {
+                actualOperand = item
+            }
+            else -> operand?.toInt()
+        }
+        // Could be + or *
+        when (operator) {
+            "+" -> {
+                return item + actualOperand
+            }
+            "*" -> {
+                return item * actualOperand
+            }
+        }
+        return -1
+    }
 }
