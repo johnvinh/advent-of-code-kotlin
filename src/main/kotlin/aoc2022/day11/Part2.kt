3db721e5317bee60d17ace9790771069407a2495
John Vinh <john.michael.vinh2@gmail.com>
2022-12-11 12:33:52 -0500

    Start 2022 day 11 part 2

---
+++ src/main/kotlin/aoc2022/day11/Part2.kt
@@ -0,0 +1,18 @@
+package aoc2022.day11
+
+import dev.johnvinh.getInput
+import java.math.BigInteger
+
+fun main() {
+    val input = getInput()
+    val monkeys = createMonkeyList(input)
+    playRounds(monkeys, 10000)
+    monkeys.sortByDescending{monkey -> monkey.numInspections}
+    for (monkey in monkeys) {
+        println(monkey.numInspections)
+    }
+    //println(monkeys[0].numInspections * monkeys[1].numInspections)
+    val bigInteger1 = BigInteger(monkeys[0].numInspections.toString())
+    val bigInteger2 = BigInteger(monkeys[1].numInspections.toString())
+    println(bigInteger1.multiply(bigInteger2))
+}
