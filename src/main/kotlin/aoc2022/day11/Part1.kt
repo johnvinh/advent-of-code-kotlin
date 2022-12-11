40e6f9ad625b65900bcab7f730ff3189bc926870
John Vinh <john.michael.vinh2@gmail.com>
2022-12-11 10:29:44 -0500

    Fix getItemWorryIncrease

--- src/main/kotlin/aoc2022/day11/Part1.kt
+++ src/main/kotlin/aoc2022/day11/Part1.kt
@@ -3,16 +3,15 @@ package aoc2022.day11
 class Monkey(val items: ArrayList<Int>, private val operation: String,
              val test: String, val trueAction: String, val falseAction: String) {
     fun getItemWorryIncrease(item: Int): Int {
-        val operationRegex = Regex("new = old ([+-]) (.+)$")
+        val operationRegex = Regex("^new = old ([+*]) (.+)$")
         val match = operationRegex.find(operation)
         val operator = match?.groups?.get(1)?.value
         val operand = match?.groups?.get(2)?.value
-        var actualOperand = 0
-        when (operand) {
+        var actualOperand = when (operand) {
             "old" -> {
-                actualOperand = item
+                item
             }
-            else -> operand?.toInt()
+            else -> operand?.toInt()!!
         }
         // Could be + or *
         when (operator) {
