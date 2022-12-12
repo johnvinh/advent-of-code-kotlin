65b1343ba48286ee509bd4706cb52704b4470475
John Vinh <john.michael.vinh2@gmail.com>
2022-12-12 17:14:12 -0500

    Create getNextLocation method and start tests

--- src/main/kotlin/day12/Part1.kt
+++ src/main/kotlin/day12/Part1.kt
@@ -1,2 +1,5 @@
 package day12
 
+fun getNextLocation(row: Int, col: Int): Array<Int> {
+    return arrayOf(row, col)
+}
---
+++ src/test/kotlin/day12/Part1KtTest.kt
@@ -0,0 +1,5 @@
+package day12
+
+import org.junit.jupiter.api.Assertions.*
+
+class Part1KtTest
