46038d140f1d4caebafbb2358085ad6e394d17e7
John Vinh <john.michael.vinh2@gmail.com>
2022-12-12 17:20:18 -0500

    Create getStartLocation method

--- src/main/kotlin/day12/Part1.kt
+++ src/main/kotlin/day12/Part1.kt
@@ -3,3 +3,14 @@ package day12
 fun getNextLocation(row: Int, col: Int): Array<Int> {
     return arrayOf(row, col)
 }
+
+fun getStartLocation(grid: Array<Array<Char>>, row: Int, col: Int): Array<Int> {
+    for (i in grid.indices) {
+        for (j in grid[i].indices) {
+            if (grid[i][j] == 'S') {
+                return arrayOf(i, j)
+            }
+        }
+    }
+    return arrayOf(-1, -1)
+}
