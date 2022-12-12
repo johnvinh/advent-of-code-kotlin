66f4d976d90ea244881ae33e86dc6c41865fd09a
John Vinh <john.michael.vinh2@gmail.com>
2022-12-12 18:44:06 -0500

    Start getShortestPath and getNeighbors

--- src/main/kotlin/day12/Part1.kt
+++ src/main/kotlin/day12/Part1.kt
@@ -1,6 +1,11 @@
 package day12
 
-fun getStartOrEndLocation(start: Boolean, grid: Array<String>, row: Int, col: Int): Array<Int> {
+import java.util.LinkedList
+import java.util.Queue
+
+class Node(val x: Int, val y: Int, val parent: Node?)
+
+fun getStartOrEndLocation(start: Boolean, grid: Array<String>): Array<Int> {
     val searchTarget = if (start) {
         'S'
     } else {
@@ -16,6 +21,32 @@ fun getStartOrEndLocation(start: Boolean, grid: Array<String>, row: Int, col: In
     return arrayOf(-1, -1)
 }
 
-fun getShortestPathDistance(grid: Array<String>): Int {
-    return 0
+fun getNeighbors(grid: Array<String>, node: Node): ArrayList<Node> {
+    val out = ArrayList<Node>()
+    // Above
+    // Left
+    // Right
+    // Below
+    return ArrayList()
+}
+
+fun getShortestPath(grid: Array<String>): Node {
+    // (x, y) coordinates
+    val startLocation = getStartOrEndLocation(true, grid)
+    val endLocation = getStartOrEndLocation(false, grid)
+
+    // Breadth-first search
+    val queue = LinkedList<Node>()
+    val visited = HashSet<Node>()
+    val startNode = Node(startLocation[0], startLocation[1], null)
+    queue.add(startNode)
+    visited.add(startNode)
+
+    while (queue.isNotEmpty()) {
+        val v = queue.remove()
+        if (v.x == endLocation[0] && v.y == endLocation[1]) {
+            return v
+        }
+    }
+    return startNode
 }
