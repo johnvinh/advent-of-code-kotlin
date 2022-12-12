d81778703bed49a16ebd3155f5de19baafb36dbc
John Vinh <john.michael.vinh2@gmail.com>
2022-12-12 18:19:08 -0500

    Start tests for getShortestPathDistance

--- src/test/kotlin/day12/Part1KtTest.kt
+++ src/test/kotlin/day12/Part1KtTest.kt
@@ -1,5 +1,18 @@
 package day12
 
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
 
-class Part1KtTest
+class Part1KtTest {
+    private val exampleGrid = arrayOf(
+        "Sabqponm",
+        "abcryxxl",
+        "accszExk",
+        "acctuvwj",
+        "abdefghi",
+    )
+    @Test
+    fun getShortestPathDistance_ExampleInput_31() {
+        assertEquals(31, getShortestPathDistance(exampleGrid))
+    }
+}
