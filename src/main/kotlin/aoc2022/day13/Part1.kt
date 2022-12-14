d8f4ba1085d4e9fe339c7fb69f78ae8876c611d9
John Vinh <john.michael.vinh2@gmail.com>
2022-12-13 19:25:25 -0500

    Create isInRightOrder function for two integers

--- src/main/kotlin/aoc2022/day13/Part1.kt
+++ src/main/kotlin/aoc2022/day13/Part1.kt
@@ -1,2 +1,12 @@
 package aoc2022.day13
 
+fun isInRightOrder(num1: Int, num2: Int): Array<Boolean> {
+    // value 1: is in right order
+    // value 2: continue checking
+    if (num1 == num2) {
+        return arrayOf(false, true) // keep checking
+    } else if (num1 < num2) {
+        return arrayOf(true, false) // right order
+    }
+    return arrayOf(false, false) // incorrect order
+}
