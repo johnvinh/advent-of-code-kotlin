1c4c1adc7d6913462a173b728291229873273545
John Vinh <john.michael.vinh2@gmail.com>
2022-12-16 20:15:29 -0500

    Create test cases for first two examples

--- src/test/kotlin/aoc2015/day5/Part1KtTest.kt
+++ src/test/kotlin/aoc2015/day5/Part1KtTest.kt
@@ -1,5 +1,16 @@
 package aoc2015.day5
 
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
 
-class Part1KtTest
+class Part1KtTest {
+    @Test
+    fun isANiceString_example1() {
+        assertTrue(isANiceString("ugknbfddgicrmopn"))
+    }
+
+    @Test
+    fun isANiceString_example2() {
+        assertTrue(isANiceString("aaa"))
+    }
+}
