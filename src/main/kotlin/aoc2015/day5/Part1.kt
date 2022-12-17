bde82ce8c5e6e009d950900eba3d7b5b7d75dce5
John Vinh <john.michael.vinh2@gmail.com>
2022-12-16 20:25:49 -0500

    Solve 2015 day 5 part 1

--- src/main/kotlin/aoc2015/day5/Part1.kt
+++ src/main/kotlin/aoc2015/day5/Part1.kt
@@ -1,5 +1,42 @@
 package aoc2015.day5
 
+import dev.johnvinh.getInput
+
 fun isANiceString(s: String): Boolean {
-    return false
+    val vowels = HashSet<Char>()
+    vowels.add('a')
+    vowels.add('e')
+    vowels.add('i')
+    vowels.add('o')
+    vowels.add('u')
+
+    if (s.contains("ab") || s.contains("cd") || s.contains("pq") || s.contains("xy")) {
+        return false
+    }
+    var vowelCount = 0
+    var hasDoubleLetter = false
+    for (i in s.indices) {
+        val c = s[i]
+        if (i != s.length-1 && c == s[i + 1]) {
+            hasDoubleLetter = true
+        }
+        if (vowels.contains(c)) {
+            vowelCount++
+        }
+    }
+    if (vowelCount < 3) {
+        return false
+    }
+    return hasDoubleLetter
+}
+
+fun main() {
+    val lines = getInput()
+    var niceCounter = 0
+    for (line in lines) {
+        if (isANiceString(line)) {
+            niceCounter++
+        }
+    }
+    println("Number of nice strings: $niceCounter")
 }
