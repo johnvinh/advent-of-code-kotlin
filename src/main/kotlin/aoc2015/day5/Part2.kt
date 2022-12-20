f1f0b05928a8b824a2c64220d3a33640c2422f30
John Vinh <john.michael.vinh2@gmail.com>
2022-12-19 22:30:37 -0500

    Solve 2015 day 5 part 2 test example cases

---
+++ src/main/kotlin/aoc2015/day5/Part2.kt
@@ -0,0 +1,37 @@
+package aoc2015.day5
+
+import dev.johnvinh.getInput
+
+fun isANiceString2(s: String): Boolean {
+    var hasRepeatingPair = false
+    var repeatingLetterOneInbetween = false
+    val repeatingIndices = HashSet<Int>()
+
+    for (i in s.indices) {
+        if ((i != s.length-1) && (s[i] == s[i + 1]) && !hasRepeatingPair) {
+            hasRepeatingPair = true
+            repeatingIndices.add(i)
+            repeatingIndices.add(i + 1)
+        } else if ((i != s.length-1) && (s[i] == s[i + 1]) && hasRepeatingPair) {
+            if (repeatingIndices.contains(i) || repeatingIndices.contains(i + 1)) {
+                return false
+            }
+        }
+
+        if (((i + 2) < s.length) && (s[i] != s[i + 1]) && (s[i] == s[i + 2])) {
+            repeatingLetterOneInbetween = true
+        }
+    }
+    return hasRepeatingPair && repeatingLetterOneInbetween
+}
+
+fun main() {
+    val lines = getInput()
+    var niceStrings = 0
+    for (line in lines) {
+        if (isANiceString2(line)) {
+            niceStrings++
+        }
+    }
+    println(niceStrings)
+}
