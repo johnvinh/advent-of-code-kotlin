15c84e30fa60737326c9205055e588dccbbc9f8a
John Vinh <john.michael.vinh2@gmail.com>
2022-12-21 09:04:37 -0500

    Finish createList function

--- src/main/kotlin/aoc2022/day20/Part1.kt
+++ src/main/kotlin/aoc2022/day20/Part1.kt
@@ -1,5 +1,13 @@
 package aoc2022.day20
 
+fun createList(input: List<String>): IntArray {
+    val out = IntArray(input.size){0}
+    for (i in input.indices) {
+        out[i] = input[i].toInt()
+    }
+    return out
+}
+
 fun moveNumber(indice: Int, numbers: List<Int>) {
-    
+
 }
