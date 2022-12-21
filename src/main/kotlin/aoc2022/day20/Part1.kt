b7139b7e15cb1563a8d22b1732fa38410ce11660
John Vinh <john.michael.vinh2@gmail.com>
2022-12-21 09:59:15 -0500

    Make progress on mixNumbers

--- src/main/kotlin/aoc2022/day20/Part1.kt
+++ src/main/kotlin/aoc2022/day20/Part1.kt
@@ -1,5 +1,8 @@
 package aoc2022.day20
 
+import dev.johnvinh.getInput
+import kotlin.math.abs
+
 fun createList(input: List<String>): IntArray {
     val out = IntArray(input.size){0}
     for (i in input.indices) {
@@ -8,6 +11,43 @@ fun createList(input: List<String>): IntArray {
     return out
 }
 
-fun moveNumber(indice: Int, numbers: IntArray) {
+fun mixNumbers(numbers: IntArray): IntArray {
+    val numbersCopy = numbers.copyOf()
+    val numberCurrentIndices = HashMap<Int, Int>()
+    for (i in numbers.indices) {
+        numberCurrentIndices[numbers[i]] = i
+    }
+    for (i in numbersCopy.indices) {
+        val currentNumber = numbers[i]
+        val currentNumberIndice = numberCurrentIndices[currentNumber]!!
+        if (currentNumber >= 0) {
+            val newIndice = (currentNumberIndice + currentNumber) % numbers.size
+            val newIndiceValue = numbersCopy[newIndice]
+            numbersCopy[newIndice] = currentNumber
+            numbersCopy[i] = newIndiceValue
+            numberCurrentIndices[currentNumber] = newIndice
+        } else {
+            val newIndice = numbers.size - abs(currentNumber)
+            val newIndiceValue = numbersCopy[newIndice]
+            numbersCopy[newIndice] = currentNumber
+            numbersCopy[i] = newIndiceValue
+            numberCurrentIndices[currentNumber] = newIndice
+        }
+    }
+    return numbersCopy
+}
 
+fun main() {
+    val input = getInput()
+    var numbers = createList(input)
+    numbers = mixNumbers(numbers)
+    print("[")
+    for (i in numbers.indices) {
+        if (i == numbers.size-1) {
+            print(numbers[i])
+        } else {
+            print("${numbers[i]}, ")
+        }
+    }
+    print("]")
 }
