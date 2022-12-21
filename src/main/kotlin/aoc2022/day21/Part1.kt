359ce531ab1198a2eca1a898873991148538f702
John Vinh <john.michael.vinh2@gmail.com>
2022-12-21 10:13:02 -0500

    Create regex for the four different cases

--- src/main/kotlin/aoc2022/day21/Part1.kt
+++ src/main/kotlin/aoc2022/day21/Part1.kt
@@ -1,2 +1,13 @@
 package aoc2022.day21
 
+fun createMap(input: List<String>): HashMap<String, Int> {
+    val constantRegex = Regex("^([A-Za-z]+?): ([0-9]+)$")
+    val addRegex = Regex("^([A-Za-z]+?): ([A-Za-z]+?) + ([A-Za-z]+?)$")
+    val subtractRegex = Regex("^([A-Za-z]+?): ([A-Za-z]+?) - ([A-Za-z]+?)$")
+    val multiplyRegex = Regex("^([A-Za-z]+?): ([A-Za-z]+?) * ([A-Za-z]+?)$")
+    val divideRegex = Regex("^([A-Za-z]+?): ([A-Za-z]+?) / ([A-Za-z]+?)$")
+    for (line in input) {
+
+    }
+    return HashMap()
+}
