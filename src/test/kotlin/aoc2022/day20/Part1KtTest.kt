448ac0f0f31b59e25d038557483fd42ba9c9be3e
John Vinh <john.michael.vinh2@gmail.com>
2022-12-21 09:08:43 -0500

    Create test for createList function on example input

--- src/test/kotlin/aoc2022/day20/Part1KtTest.kt
+++ src/test/kotlin/aoc2022/day20/Part1KtTest.kt
@@ -1,5 +1,14 @@
 package aoc2022.day20
 
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
 
-class Part1KtTest
+class Part1KtTest {
+    private val exampleInput = arrayListOf("1", "2", "-3", "3", "-2", "0", "4")
+    @Test
+    fun createList_Example() {
+        val expected = intArrayOf(1, 2, -3, 3, -2, 0, 4)
+        val actual = createList(exampleInput)
+        assertArrayEquals(expected, actual)
+    }
+}
