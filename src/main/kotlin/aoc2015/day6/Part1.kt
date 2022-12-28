e43dfd1458c67b8b8750df429b369e031d2cf440
John Vinh <john.michael.vinh2@gmail.com>
2022-12-28 18:37:24 -0500

    Start parseRange function

--- src/main/kotlin/aoc2015/day6/Part1.kt
+++ src/main/kotlin/aoc2015/day6/Part1.kt
@@ -2,8 +2,15 @@ package aoc2015.day6
 
 import dev.johnvinh.getInput
 
+fun parseRange(line: String): Array<IntArray> {
+    return Array(0){IntArray(0)}
+}
 
 fun main() {
     val lines = getInput()
-    val lights = Array(1000){BooleanArray(1000)}
+    val lights = Array(1000){BooleanArray(1000){false} }
+    for (line in lines) {
+
+    }
+
 }
