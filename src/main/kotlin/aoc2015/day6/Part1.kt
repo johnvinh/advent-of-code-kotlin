ec606485b5755611a324419a26a084c62247f3f3
John Vinh <john.michael.vinh2@gmail.com>
2022-12-28 18:45:35 -0500

    Finish parseRange

--- src/main/kotlin/aoc2015/day6/Part1.kt
+++ src/main/kotlin/aoc2015/day6/Part1.kt
@@ -3,14 +3,21 @@ package aoc2015.day6
 import dev.johnvinh.getInput
 
 fun parseRange(line: String): Array<IntArray> {
-    return Array(0){IntArray(0)}
+    val regex = Regex("([0-9]+?),([0-9]+?) through ([0-9]+?),([0-9]+)")
+    val match = regex.find(line)
+    val lowerRow = match!!.groups[1]?.value?.toInt()!!
+    val lowerCol = match.groups[2]?.value?.toInt()!!
+    val upperRow = match.groups[3]?.value?.toInt()!!
+    val upperCol = match.groups[4]?.value?.toInt()!!
+    return arrayOf(intArrayOf(lowerRow, lowerCol), intArrayOf(upperRow, upperCol))
 }
 
 fun main() {
     val lines = getInput()
     val lights = Array(1000){BooleanArray(1000){false} }
     for (line in lines) {
-
+        val range = parseRange(line)
+        println("Range: (${range[0][0]}, ${range[0][1]}) to (${range[1][0]}, ${range[1][1]})")
     }
 
 }
