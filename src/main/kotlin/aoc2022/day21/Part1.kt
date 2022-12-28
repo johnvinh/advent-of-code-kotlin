79b7d1856b400887aaf4a42c767a48885a89568d
John Vinh <john.michael.vinh2@gmail.com>
2022-12-28 18:52:45 -0500

    Start createMap helper method

--- src/main/kotlin/aoc2022/day21/Part1.kt
+++ src/main/kotlin/aoc2022/day21/Part1.kt
@@ -1,13 +1,17 @@
 package aoc2022.day21
 
-fun createMap(input: List<String>): HashMap<String, Int> {
+fun createMap(input: List<String>): HashMap<String, String> {
     val constantRegex = Regex("^([A-Za-z]+?): ([0-9]+)$")
     val addRegex = Regex("^([A-Za-z]+?): ([A-Za-z]+?) + ([A-Za-z]+?)$")
     val subtractRegex = Regex("^([A-Za-z]+?): ([A-Za-z]+?) - ([A-Za-z]+?)$")
     val multiplyRegex = Regex("^([A-Za-z]+?): ([A-Za-z]+?) * ([A-Za-z]+?)$")
     val divideRegex = Regex("^([A-Za-z]+?): ([A-Za-z]+?) / ([A-Za-z]+?)$")
     for (line in input) {
-
+        if (constantRegex.matches(line)) {
+            val match = constantRegex.matchEntire(line)
+            val key = match!!.groups[1]!!.value
+            val value = match.groups[2]!!.value
+        }
     }
     return HashMap()
 }
