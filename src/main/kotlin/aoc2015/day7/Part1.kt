41fc718e242213dd33a9b6454739ab5f6bc91305
John Vinh <john.michael.vinh2@gmail.com>
2022-12-28 20:22:10 -0500

    Solve example input

--- src/main/kotlin/aoc2015/day7/Part1.kt
+++ src/main/kotlin/aoc2015/day7/Part1.kt
@@ -16,12 +16,126 @@ fun initializeMap(lines: List<String>): HashMap<String, Int> {
     return out
 }
 
-fun calculateSignalValue(wire: String, wires: HashMap<String, Int>): Int {
-    return 0
+fun calculateSignalValue(lines: List<String>, wire: String, wires: HashMap<String, Int>): Int {
+    if (wires.containsKey(wire)) {
+        return wires[wire]!!
+    }
+
+    val andRegex = Regex("^([A-Za-z0-9]+?) AND ([A-Za-z0-9]+?) -> ([A-Za-z]+?)$")
+    val orRegex = Regex("^([A-Za-z0-9]+?) OR ([A-Za-z0-9]+?) -> ([A-Za-z]+?)$")
+    val lshiftRegex = Regex("^([A-Za-z0-9]+?) LSHIFT ([A-Za-z0-9]+?) -> ([A-Za-z]+?)$")
+    val rshiftRegex = Regex("^([A-Za-z0-9]+?) RSHIFT ([A-Za-z0-9]+?) -> ([A-Za-z]+?)$")
+    val notRegex = Regex("^NOT ([A-Za-z]+?) -> ([A-Za-z]+?)$")
+
+    for (line in lines) {
+        // Find the assignment line
+        if (line.contains("-> $wire")) {
+            val andMatch = andRegex.matchEntire(line)
+            val orMatch = orRegex.matchEntire(line)
+            val lshiftMatch = lshiftRegex.matchEntire(line)
+            val rshiftMatch = rshiftRegex.matchEntire(line)
+            val notMatch = notRegex.matchEntire(line)
+            if (andMatch != null) {
+                val operand1 = andMatch.groups[1]?.value!!
+                val operand2 = andMatch.groups[2]?.value!!
+
+                val operand1Value: Int = if (operand1.toIntOrNull() == null) {
+                    calculateSignalValue(lines, operand1, wires)
+                } else {
+                    operand1.toInt()
+                }
+                wires[operand1] = operand1Value
+
+                val operand2Value: Int = if (operand2.toIntOrNull() == null) {
+                    calculateSignalValue(lines, operand2, wires)
+                } else {
+                    operand2.toInt()
+                }
+                wires[operand2] = operand2Value
+
+                val finalResult = operand1Value and operand2Value
+                wires[wire] = finalResult
+                return finalResult
+            } else if (orMatch != null) {
+                val operand1 = orMatch.groups[1]?.value!!
+                val operand2 = orMatch.groups[2]?.value!!
+
+                val operand1Value: Int = if (operand1.toIntOrNull() == null) {
+                    calculateSignalValue(lines, operand1, wires)
+                } else {
+                    operand1.toInt()
+                }
+                wires[operand1] = operand1Value
+
+                val operand2Value: Int = if (operand2.toIntOrNull() == null) {
+                    calculateSignalValue(lines, operand2, wires)
+                } else {
+                    operand2.toInt()
+                }
+                wires[operand2] = operand2Value
+
+                val finalResult = operand1Value or operand2Value
+                wires[wire] = finalResult
+                return finalResult
+            } else if (lshiftMatch != null) {
+                val operand1 = lshiftMatch.groups[1]?.value!!
+                val operand2 = lshiftMatch.groups[2]?.value!!
+
+                val operand1Value: Int = if (operand1.toIntOrNull() == null) {
+                    calculateSignalValue(lines, operand1, wires)
+                } else {
+                    operand1.toInt()
+                }
+                wires[operand1] = operand1Value
+
+                val operand2Value: Int = if (operand2.toIntOrNull() == null) {
+                    calculateSignalValue(lines, operand2, wires)
+                } else {
+                    operand2.toInt()
+                }
+                wires[operand2] = operand2Value
+
+                val finalResult = operand1Value shl operand2Value
+                wires[wire] = finalResult
+                return finalResult
+            } else if (rshiftMatch != null) {
+                val operand1 = rshiftMatch.groups[1]?.value!!
+                val operand2 = rshiftMatch.groups[2]?.value!!
+
+                val operand1Value: Int = if (operand1.toIntOrNull() == null) {
+                    calculateSignalValue(lines, operand1, wires)
+                } else {
+                    operand1.toInt()
+                }
+
+                val operand2Value: Int = if (operand2.toIntOrNull() == null) {
+                    calculateSignalValue(lines, operand2, wires)
+                } else {
+                    operand2.toInt()
+                }
+
+                val finalResult = operand1Value shr operand2Value
+                wires[wire] = finalResult
+                return finalResult
+            } else if (notMatch != null) {
+                val operand = notMatch.groups[1]?.value!!
+                val value = if (operand.toIntOrNull() == null) {
+                    calculateSignalValue(lines, operand, wires)
+                } else {
+                    operand.toInt()
+                }
+
+                val finalResult = 65535 + value.inv() + 1
+                wires[wire] = finalResult
+                return finalResult
+            }
+        }
+    }
+    return -1
 }
 
 fun main() {
     val lines = getInput()
     val wires = initializeMap(lines)
-    println(wires)
+    println(calculateSignalValue(lines, "a", wires))
 }
