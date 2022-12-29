be8fd212396af252ae8dc127ef8af3b7dcea9b07
John Vinh <john.michael.vinh2@gmail.com>
2022-12-28 19:00:21 -0500

    Create parseInstruction for second part

--- src/main/kotlin/aoc2015/day6/Part2.kt
+++ src/main/kotlin/aoc2015/day6/Part2.kt
@@ -1,2 +1,15 @@
 package aoc2015.day6
 
+import dev.johnvinh.getInput
+
+fun parseInstruction2(line: String, range: Array<IntArray>, lights: Array<IntArray>, row: Int, col: Int) {
+    if (line.contains("turn on")) {
+        lights[row][col]++
+    } else if (line.contains("turn off")) {
+        if (lights[row][col] > 0) {
+            lights[row][col]--
+        }
+    } else {
+        lights[row][col] += 2
+    }
+}
