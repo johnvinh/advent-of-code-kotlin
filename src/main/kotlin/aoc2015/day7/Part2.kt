044694030b3e70cfd28b823d22961482728bf94c
John Vinh <john.michael.vinh2@gmail.com>
2022-12-29 13:29:17 -0500

    Solve 2015 day 7 part 2

--- src/main/kotlin/aoc2015/day7/Part2.kt
+++ src/main/kotlin/aoc2015/day7/Part2.kt
@@ -1,2 +1,14 @@
 package aoc2015.day7
 
+import dev.johnvinh.getInput
+
+fun main() {
+    val lines = getInput()
+    val part1 = Part1(lines)
+    part1.initializeMap(lines)
+    val a = part1.calculateSignalValue("a")
+    part1.wires.clear()
+    part1.wires["b"] = a
+    val newA = part1.calculateSignalValue("a")
+    println(newA)
+}
