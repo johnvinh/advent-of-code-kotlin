5b6eac536375b71802f7982cf0f615c604396d16
John Vinh <john.michael.vinh2@gmail.com>
2022-12-28 19:23:02 -0500

    Finish initializeMap method

--- src/main/kotlin/aoc2015/day7/Part1.kt
+++ src/main/kotlin/aoc2015/day7/Part1.kt
@@ -1,5 +1,23 @@
 package aoc2015.day7
 
+import dev.johnvinh.getInput
+
 fun initializeMap(lines: List<String>): HashMap<String, Int> {
-    return HashMap()
+    val out = HashMap<String, Int>()
+    val constantRegex = Regex("^([0-9]+?) -> ([A-Za-z]+)$")
+    for (line in lines) {
+        val match = constantRegex.find(line)
+        if (match != null) {
+            val signal = match.groups[1]?.value?.toInt()!!
+            val wire = match.groups[2]?.value!!
+            out[wire] = signal
+        }
+    }
+    return out
+}
+
+fun main() {
+    val lines = getInput()
+    val wires = initializeMap(lines)
+    println(wires)
 }
