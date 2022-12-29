52d8ec09446d121871cc875e2f73057a050b79ac
John Vinh <john.michael.vinh2@gmail.com>
2022-12-28 19:01:28 -0500

    Solve 2015 day 6 part 2

--- src/main/kotlin/aoc2015/day6/Part2.kt
+++ src/main/kotlin/aoc2015/day6/Part2.kt
@@ -13,3 +13,27 @@ fun parseInstruction2(line: String, range: Array<IntArray>, lights: Array<IntArr
         lights[row][col] += 2
     }
 }
+
+fun main() {
+    val lines = getInput()
+    val lights = Array(1000){IntArray(1000){0} }
+    for (line in lines) {
+        val range = parseRange(line)
+        // Row
+        for (i in range[0][0]..range[1][0]) {
+            // Col
+            for (j in range[0][1]..range[1][1]) {
+                parseInstruction2(line, range, lights, i, j)
+            }
+        }
+    }
+
+    // Get number of lights still on
+    var brightnessSum = 0
+    for (i in lights.indices) {
+        for (j in lights[i].indices) {
+            brightnessSum += lights[i][j]
+        }
+    }
+    println("Brightness: $brightnessSum")
+}
