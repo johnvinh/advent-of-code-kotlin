9f694613c7d067760cbe400fe75663ed8f3c2ef3
John Vinh <john.michael.vinh2@gmail.com>
2022-12-30 21:46:08 -0500

    Remove extra regexes

--- src/main/kotlin/aoc2022/day21/Part1.kt
+++ src/main/kotlin/aoc2022/day21/Part1.kt
@@ -1,19 +1,17 @@
 package aoc2022.day21
 
 import dev.johnvinh.getInput
+import java.lang.IllegalArgumentException
 import java.math.BigInteger
 
-enum class BinaryOperation {
-    ADD, SUBTRACT, MULTIPLY, DIVIDE
-}
-
 fun calculateBinaryOperation(
-    operation: BinaryOperation, regex: Regex, monkeys: HashMap<String, BigInteger>,
+    regex: Regex, monkeys: HashMap<String, BigInteger>,
     line: String, monkey: String, lines: List<String>
 ): BigInteger {
     val match = regex.matchEntire(line)
     val monkey1Name = match!!.groups[2]!!.value
-    val monkey2Name = match.groups[3]!!.value
+    val operation = match.groups[3]!!.value
+    val monkey2Name = match.groups[4]!!.value
     val monkey1Value = if (monkeys.containsKey(monkey1Name)) {
         monkeys[monkey1Name]
     } else {
@@ -26,17 +24,18 @@ fun calculateBinaryOperation(
     }
 
     val result = when (operation) {
-        BinaryOperation.ADD -> BigInteger.valueOf(monkey1Value!!.toLong())
+        "+" -> BigInteger.valueOf(monkey1Value!!.toLong())
             .add(BigInteger.valueOf(monkey2Value!!.toLong()))
 
-        BinaryOperation.SUBTRACT -> BigInteger.valueOf(monkey1Value!!.toLong())
+        "-" -> BigInteger.valueOf(monkey1Value!!.toLong())
             .subtract(BigInteger.valueOf(monkey2Value!!.toLong()))
 
-        BinaryOperation.DIVIDE -> BigInteger.valueOf(monkey1Value!!.toLong())
+        "/" -> BigInteger.valueOf(monkey1Value!!.toLong())
             .divide(BigInteger.valueOf(monkey2Value!!.toLong()))
 
-        BinaryOperation.MULTIPLY -> BigInteger.valueOf(monkey1Value!!.toLong())
+        "*" -> BigInteger.valueOf(monkey1Value!!.toLong())
             .multiply(BigInteger.valueOf(monkey2Value!!.toLong()))
+        else -> throw IllegalArgumentException()
     }
     monkeys[monkey] = result
     return result
@@ -48,10 +47,7 @@ fun calculateMonkeyNumber(lines: List<String>, monkey: String, monkeys: HashMap<
     }
 
     val constantRegex = Regex("^([A-Za-z]+?): ([0-9]+)$")
-    val addRegex = Regex("^([A-Za-z]+?): ([A-Za-z]+?) \\+ ([A-Za-z]+?)$")
-    val subtractRegex = Regex("^([A-Za-z]+?): ([A-Za-z]+?) - ([A-Za-z]+?)$")
-    val multiplyRegex = Regex("^([A-Za-z]+?): ([A-Za-z]+?) \\* ([A-Za-z]+?)$")
-    val divideRegex = Regex("^([A-Za-z]+?): ([A-Za-z]+?) / ([A-Za-z]+?)$")
+    val binaryOperationRegex = Regex("^([A-Za-z]+?): ([A-Za-z]+?) ([+*/-]) ([A-Za-z]+?)$")
     for (line in lines) {
         if (!(line.contains("${monkey}:"))) {
             continue
@@ -62,14 +58,8 @@ fun calculateMonkeyNumber(lines: List<String>, monkey: String, monkeys: HashMap<
             val value = match.groups[2]!!.value
             monkeys[key] = BigInteger.valueOf(value.toLong())
             return BigInteger.valueOf(value.toLong())
-        } else if (addRegex.matches(line)) {
-            return calculateBinaryOperation(BinaryOperation.ADD, addRegex, monkeys, line, monkey, lines)
-        } else if (subtractRegex.matches(line)) {
-            return calculateBinaryOperation(BinaryOperation.SUBTRACT, subtractRegex, monkeys, line, monkey, lines)
-        } else if (multiplyRegex.matches(line)) {
-            return calculateBinaryOperation(BinaryOperation.MULTIPLY, multiplyRegex, monkeys, line, monkey, lines)
-        } else if (divideRegex.matches(line)) {
-            return calculateBinaryOperation(BinaryOperation.DIVIDE, divideRegex, monkeys, line, monkey, lines)
+        } else if (binaryOperationRegex.matches(line)) {
+            return calculateBinaryOperation(binaryOperationRegex, monkeys, line, monkey, lines)
         }
     }
     return BigInteger.ZERO
