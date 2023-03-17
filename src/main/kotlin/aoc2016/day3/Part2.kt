54613328e9628dec4bc09f2702056beced0a8ac9
John Vinh <john.michael.vinh2@gmail.com>
2023-03-16 23:29:18 -0400

    Solve 2016 day 3 part 2

--- src/main/kotlin/aoc2016/day3/Part2.kt
+++ src/main/kotlin/aoc2016/day3/Part2.kt
@@ -1,2 +1,27 @@
 package aoc2016.day3
 
+import dev.johnvinh.getInput
+
+fun parseInput2(sideSets: List<List<Int>>): MutableList<List<Int>> {
+    val outList: MutableList<List<Int>> = mutableListOf()
+
+    for (i in sideSets.indices step 3) {
+        for (j in 0..2) {
+            outList.add(mutableListOf(sideSets[i][j], sideSets[i + 1][j], sideSets[i + 2][j]))
+        }
+    }
+    return outList
+}
+
+fun main() {
+    val lines = getInput()
+    val numbers = parseInput(lines)
+    val newSides = parseInput2(numbers)
+    var numPossible = 0
+    for (sides in newSides) {
+        if (isValidTriangle(sides)) {
+            numPossible++
+        }
+    }
+    println(numPossible)
+}
