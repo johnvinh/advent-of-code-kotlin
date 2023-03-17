424437531dd9294381af081a74b2872e0b89885f
John Vinh <john.michael.vinh2@gmail.com>
2023-03-16 23:06:10 -0400

    Move parsing to separate function

--- src/main/kotlin/aoc2016/day3/Part1.kt
+++ src/main/kotlin/aoc2016/day3/Part1.kt
@@ -2,16 +2,23 @@ package aoc2016.day3
 
 import dev.johnvinh.getInput
 
-fun main() {
-    val lines = getInput()
+fun parseInput(lines: List<String>): List<List<Int>> {
     val regex = Regex("^ *([0-9]+?) +([0-9]+?) +([0-9]+)$")
+    val outList: MutableList<MutableList<Int>> = mutableListOf()
     for (line in lines) {
         val match = regex.matchEntire(line)
         if (match != null) {
-            val num1 = match.groups[1]?.value
-            val num2 = match.groups[2]?.value
-            val num3 = match.groups[3]?.value
-            println("$num1 $num2 $num3")
+            val num1 = match.groups[1]?.value?.toInt()
+            val num2 = match.groups[2]?.value?.toInt()
+            val num3 = match.groups[3]?.value?.toInt()
+            if (num1 != null && num2 != null && num3 != null) {
+                outList.add(mutableListOf(num1, num2, num3))
+            }
         }
     }
+    return outList
+}
+
+fun main() {
+    val lines = getInput()
 }
